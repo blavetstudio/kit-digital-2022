8247ff3a81d0231656502113648b93d9e5b3d1c4
Carlos Faria <carlos@blavetstudio.com>
2022-03-29 16:15:39 +0200

    Fix margen inferior Soluciones

--- pages/index.js
+++ pages/index.js
@@ -256,12 +256,10 @@ export default function Home() {
 
         {/* Soluciones */}
         <section id="soluciones" className="w-full bg-white py-7 md:py-12">
-                <h3 className="mt-1 mb-8 text-2xl font-bold text-left text-gray-800 sm:mx-6 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0">
-                    Soluciones
-                </h3>
+            <h3 className="mt-1 mb-12 text-2xl font-bold text-left text-gray-800 sm:mx-0 sm:text-3xl sm:text-center md:text-4xl lg:text-5xl">
+                Soluciones
+            </h3>
             <div className="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
-
-
                 <div className="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
                     <figure className="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20">
                       <Image alt="" src={productivity}></Image>
@@ -275,7 +273,7 @@ export default function Home() {
                     <p className="pt-4 pb-4 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
                         Dentro de la asignación del bono digital podrás optar, a través de agentes digitalizadores, a uno o varios de estos servicios: Sitio Web y Presencia en Internet, Comercio electrónico, Gestión de redes sociales, Gestión de clientes, BI y Analítica, Servicios de oficinas virtuales, Gestión de procesos, Factura electrónica, Comunicaciones seguras y Ciberseguridad.
                         <br /><br />
-                        Nosotros te podemos ayudar en dos áreas: <a href="#solucion-web" className='text-yellow-600 hover:text-yellow-500' rel="nofollow noopener noreferrer">Sitio Web y Presencia en Internet</a>, <a href="#solucion-ecommerce" className='text-yellow-600 hover:text-yellow-500' rel="nofollow noopener noreferrer">Comercio electrónico</a>
+                        Nosotros te podemos ayudar en dos áreas:<br/> <a href="#solucion-web" className='text-yellow-600 hover:text-yellow-500' rel="nofollow noopener noreferrer">Sitio Web y Presencia en Internet</a> <br/> <a href="#solucion-ecommerce" className='text-yellow-600 hover:text-yellow-500' rel="nofollow noopener noreferrer">Comercio electrónico</a>
                     </p>
                 </div>
 
